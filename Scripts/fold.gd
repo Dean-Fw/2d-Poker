0dba6991f48fac868e99269634d1263bc4ab7636
Dean Williams <dean@dean-pc>
2024-01-29 21:42:29 +0000

    Fixed folding issue

--- Scripts/fold.gd
+++ Scripts/fold.gd
@@ -3,12 +3,12 @@ extends Button
 const placeholder_texture = preload("res://Scenes/Card/CardImages/Placeholder.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.pressed(self._fold)
+	self._pressed()
 
-func _fold():
+func _pressed():
 	var cards: Array[CardClass] = [get_node("../../Cards/Card1"), get_node("../../Cards/Card2")]
 	for card in cards:
 		card.texture = placeholder_texture
 		card.value = 0
 		card.suit = ""
-	
+	self.release_focus()
